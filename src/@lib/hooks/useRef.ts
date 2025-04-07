5ebaa827ed00820882686d7e332156c6bc8d91a7
zlrlo <easy10612@gmail.com>
2025-04-08 01:02:51 +0900

    feat: useRef hook 구현
    - basic 테스트코드 완료

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export function useRef<T>(initialValue: T): { current: T } {
-  // React의 useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+  const [ref] = useState({ current: initialValue });
+
+  return ref;
 }
