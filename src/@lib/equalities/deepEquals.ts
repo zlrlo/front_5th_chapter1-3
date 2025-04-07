fcaea187e477590875eed06525f193f3efac095b
zlrlo <easy10612@gmail.com>
2025-04-08 00:21:26 +0900

    feat: deepEquals, shallowEquals 함수 구현
    - basic 테스트코드 완료

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,39 @@
 export function deepEquals<T>(objA: T, objB: T): boolean {
+  if (typeof objA !== typeof objB) {
+    return false;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+
+    return objA.every((item, index) => deepEquals(item, objB[index]));
+  }
+
+  if (typeof objA === "object" && typeof objB === "object") {
+    if (objA === null || objB === null) {
+      return objA === objB;
+    }
+
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+
+    return keysA.every((key) => {
+      const keyA = key as keyof typeof objA;
+      const keyB = key as keyof typeof objB;
+
+      if (!keysB.includes(key)) {
+        return false;
+      }
+
+      return deepEquals(objA[keyA], objB[keyB]);
+    });
+  }
+
   return objA === objB;
 }
--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,49 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
+  if (typeof objA !== typeof objB) {
+    return false;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+
+    return objA.every((item, index) => {
+      if (typeof item === "object" || typeof objB[index] === "object") {
+        return objA === objB;
+      }
+
+      return shallowEquals(item, objB[index]);
+    });
+  }
+
+  if (typeof objA === "object" && typeof objB === "object") {
+    if (objA === null || objB === null) {
+      return objA === objB;
+    }
+
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+
+    return keysA.every((key) => {
+      const keyA = key as keyof typeof objA;
+      const keyB = key as keyof typeof objB;
+
+      if (!keysB.includes(key)) {
+        return false;
+      }
+
+      if (typeof objA[keyA] === "object" || typeof objB[keyB] === "object") {
+        return objA[keyA] === objB[keyB];
+      }
+
+      return shallowEquals(objA[keyA], objB[keyB]);
+    });
+  }
+
   return objA === objB;
 }
